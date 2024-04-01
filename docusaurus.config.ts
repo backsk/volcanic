666010cb3d1d3a999b2c8da56d1b89e3dbc8d02c
volcanic <1605090219@qq.com>
2024-04-01 16:02:14 +0800

    build: :hammer_and_wrench: deploy by github action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,12 @@ const config: Config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: false,
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
         theme: {
           customCss: "./src/css/custom.css",
         },
